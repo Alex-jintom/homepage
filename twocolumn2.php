c5042381964817ff0bcdc8ead095a126f9ab4609
테스트 <test@gmail.com>
2023-09-21 13:56:21 +0900

    092111

--- twocolumn2.php
+++ twocolumn2.php
@@ -97,6 +97,82 @@
                     </div>
                 </section>
 
+
+      <!-- Footer -->
+      <section id="footer">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-8 col-12-medium">
+
+                                <!-- Links -->
+                                    <section>
+                                        <h2>Links to Important Stuff</h2>
+                                        <div>
+                                            <div class="row">
+                                                <div class="col-3 col-12-small">
+                                                    <ul class="link-list last-child">
+                                                        <li><a href="#">Neque amet dapibus</a></li>
+                                               
+                                                    </ul>
+                                                </div>
+                                                <div class="col-3 col-12-small">
+                                                    <ul class="link-list last-child">
+                                                        <li><a href="#">Neque amet dapibus</a></li>
+                                                      
+                                                    </ul>
+                                                </div>
+                                                <div class="col-3 col-12-small">
+                                                    <ul class="link-list last-child">
+                                                        <li><a href="#">Neque amet dapibus</a></li>
+                                                
+                                                    </ul>
+                                                </div>
+                                                <div class="col-3 col-12-small">
+                                                    <ul class="link-list last-child">
+                                                        <li><a href="#">Neque amet dapibus</a></li>
+                                              
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </section>
+
+                            </div>
+                            <div class="col-4 col-12-medium imp-medium">
+
+                                <!-- Blurb -->
+                                    <section>
+                                        <h2>An Informative Text Blurb</h2>
+                                        <p>
+                                            Duis neque nisi, dapibus sed mattis quis, rutrum accumsan sed. Suspendisse eu
+                                         
+                                        </p>
+                                    </section>
+
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            <!-- Copyright -->
+                <div id="copyright">
+                    © Untitled. All rights reserved. | Design: <a href="http://html5up.net">HTML5 UP</a>
+                </div>
+
+        </div>
+
+        <!-- Scripts -->
+            <script src="assets/js/jquery.min.js"></script>
+            <script src="assets/js/browser.min.js"></script>
+            <script src="assets/js/breakpoints.min.js"></script>
+            <script src="assets/js/util.js"></script>
+            <script src="assets/js/main.js"></script>
+    </body>
+
+
+    
+</html>
+
       
 <?php
                
@@ -260,78 +336,3 @@ if($firstPageNumber > $totalPage) {
 <?php
 include "footer.php";
 ?>
-
-      <!-- Footer -->
-      <section id="footer">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-8 col-12-medium">
-
-                                <!-- Links -->
-                                    <section>
-                                        <h2>Links to Important Stuff</h2>
-                                        <div>
-                                            <div class="row">
-                                                <div class="col-3 col-12-small">
-                                                    <ul class="link-list last-child">
-                                                        <li><a href="#">Neque amet dapibus</a></li>
-                                               
-                                                    </ul>
-                                                </div>
-                                                <div class="col-3 col-12-small">
-                                                    <ul class="link-list last-child">
-                                                        <li><a href="#">Neque amet dapibus</a></li>
-                                                      
-                                                    </ul>
-                                                </div>
-                                                <div class="col-3 col-12-small">
-                                                    <ul class="link-list last-child">
-                                                        <li><a href="#">Neque amet dapibus</a></li>
-                                                
-                                                    </ul>
-                                                </div>
-                                                <div class="col-3 col-12-small">
-                                                    <ul class="link-list last-child">
-                                                        <li><a href="#">Neque amet dapibus</a></li>
-                                              
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </section>
-
-                            </div>
-                            <div class="col-4 col-12-medium imp-medium">
-
-                                <!-- Blurb -->
-                                    <section>
-                                        <h2>An Informative Text Blurb</h2>
-                                        <p>
-                                            Duis neque nisi, dapibus sed mattis quis, rutrum accumsan sed. Suspendisse eu
-                                         
-                                        </p>
-                                    </section>
-
-                            </div>
-                        </div>
-                    </div>
-                </section>
-
-            <!-- Copyright -->
-                <div id="copyright">
-                    © Untitled. All rights reserved. | Design: <a href="http://html5up.net">HTML5 UP</a>
-                </div>
-
-        </div>
-
-        <!-- Scripts -->
-            <script src="assets/js/jquery.min.js"></script>
-            <script src="assets/js/browser.min.js"></script>
-            <script src="assets/js/breakpoints.min.js"></script>
-            <script src="assets/js/util.js"></script>
-            <script src="assets/js/main.js"></script>
-    </body>
-
-
-    
-</html>
